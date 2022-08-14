abeb1666b0a42cc9357123cfac4f3680bf6507f5
Joseph <jnjbros2@gmail.com>
2022-08-14 13:13:01 -0700

    Squashed commit of the following:
    commit 3b8b7ff619814f297faa0fae345a5d109a792aa0
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 13:11:54 2022 -0700
    
        Moved write out of for loop
    
        dictionary keys changed during iteration fixed
    
    commit e1a14034382f32dd2abf526f595e7609f4b8608f
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 13:10:02 2022 -0700
    
        Update rep.py
    
    commit 1ad8141ade4af857fef37a88f43c21e7a22b473d
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 13:07:14 2022 -0700
    
        rep remove requires mod perms
    
    commit 8543e2933c99af143ec0d7a62a94a198497751c3
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 13:02:34 2022 -0700
    
        Update rep.py
    
    commit 0c3430b8bcc2b5fa65db70576b212e6649d4b479
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 13:00:05 2022 -0700
    
        Update rep.py
    
    commit d3376d418763ff9cd2b2651b8b685f866461fdb5
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:58:36 2022 -0700
    
        Update rep.py
    
    commit eb9927f3af40f9ee458d51240c6b7713521a25ab
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:51:56 2022 -0700
    
        Fixed some stuff
    
    commit e323fd604cfaca86f1a096eaf8b3e99a3f530301
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:48:34 2022 -0700
    
        Update rep.py
    
    commit bc678f0ef3c0bb66b3774482440dc6edfcb4ac75
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:46:45 2022 -0700
    
        Update rep.py
    
    commit 95f4efcbfef6574c4801507eda76fdbaf6ad867a
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:42:28 2022 -0700
    
        Update rep.py
    
    commit be560fc7bb471b9897dce22e517232f83b13e624
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:41:04 2022 -0700
    
        I've been editing the wrong file this whole time *facepalm*
    
    commit 41e73642d417217de8afb6cf2ef043a888f4a002
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:38:44 2022 -0700
    
        fixed path
    
    commit 99ad8b126f2709a9b347641bd25a64692737fb9d
    Author: Joseph <jnjbros2@gmail.com>
    Date:   Sun Aug 14 12:01:21 2022 -0700
    
        Fixed maybe?

--- rep/rep.py
+++ rep/rep.py
@@ -24,7 +24,13 @@ class rep(commands.Cog):
         
         )
         global jsonPath
-        jsonPath = data_manager.cog_data_path(cog_instance=rep, raw_name="reputation.json")
+        path = data_manager.cog_data_path(cog_instance=self)
+        jsonPath = path / 'reputation.json'
+        if jsonPath.exists():
+            pass
+        else:
+            with jsonPath.open("w", encoding ="utf-8") as f:
+                f.write("{}")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -39,7 +45,8 @@ class rep(commands.Cog):
                 for user in users:
                     if user.id != message.author.id:
                         id = user.id
-                        with open(jsonPath, 'r') as reputation:
+                        with open(str(jsonPath), 'r') as reputation:
+                            print(reputation)
                             try:
                                 x = json.load(reputation)
                                 for userId, userRep in x.items():
@@ -48,49 +55,49 @@ class rep(commands.Cog):
                                         newWrite = {id : currentRep}
                                         await message.channel.send("**+rep** {0} you now have: {1} Rep".format(user.name, str(currentRep)))
                                         newUser = False
-                                    if newUser:
-                                        newWrite = {id : 1}
-                                        await message.channel.send("**+rep** {0} you now have: {1} Rep".format(user.name, str(1)))
-                                    x.pop(str(id), None)
-                                    x.update(newWrite)
+                                if newUser:
+                                    newWrite = {id : 1}
+                                    await message.channel.send("**+rep** {0} you now have: {1} Rep".format(user.name, str(1)))
+                                x.pop(str(id), None)
+                                x.update(newWrite)
                             except ValueError:
                                 if x == None:
                                     x = {}
-                        with open(jsonPath, 'w') as reputationWrite:
+                        with open(str(jsonPath), 'w') as reputationWrite:
                             try:
                                 json.dump(x, reputationWrite)
                             except ValueError:
                                 print("reputation.json write failed.")
 
+    @commands.mod()
     @commands.command(name="repremove", help="Removes a amount from a users reputation")
     async def repremove(self, ctx, user: discord.Member, amount:int):
-        if ctx.author.top_role.id == 971448331874209844 or ctx.author.top_role.id == 675089464036425738 or ctx.author.top_role.id == 673670374961184768:
-            newWrite = None
-            with open(jsonPath, 'r') as reputation:
-                try:
-                    x = json.load(reputation)
-                    for userId, userRep in x.items():
-                        if userId == str(user.id):
-                            currentRep = userRep - amount
-                            newWrite = {user.id : currentRep}
-                            await ctx.send("**-rep** {0} took away {1} rep from {2}. They now have {3}".format(ctx.author.name, amount, user.name, currentRep))
-                        if newWrite != None:
-                            x.pop(str(user.id), None)
-                            x.update(newWrite)
-                        else:
-                            await ctx.send("This user already has no reputation")
-                except ValueError:
-                    print("reputation.json failed to read")
-            with open(jsonPath, 'w') as reputationWrite:
-                try:
-                    json.dump(x, reputationWrite)
-                except ValueError:
-                    print("reputation.json failed to write")
+        newWrite = None
+        with open(str(jsonPath), 'r') as reputation:
+            try:
+                x = json.load(reputation)
+                for userId, userRep in x.items():
+                    if userId == str(user.id):
+                        currentRep = userRep - amount
+                        newWrite = {user.id : currentRep}
+                        await ctx.send("**-rep** {0} took away {1} rep from {2}. They now have {3}".format(ctx.author.name, amount, user.name, currentRep))
+                if newWrite != None:
+                    x.pop(str(user.id), None)
+                    x.update(newWrite)
+                else:
+                    await ctx.send("This user already has no reputation")
+            except ValueError:
+                print("reputation.json failed to read")
+        with open(str(jsonPath), 'w') as reputationWrite:
+            try:
+                json.dump(x, reputationWrite)
+            except ValueError:
+                print("reputation.json failed to write")
 
     @commands.command(name="checkrep", help="Displays a user's reputation")
     async def checkrep(self, ctx, user: discord.Member):
         userFound = False
-        with open(jsonPath, 'r') as reputation:
+        with open(str(jsonPath), 'r') as reputation:
             try:
                 x = json.load(reputation)
                 for userId, userRep in x.items():
