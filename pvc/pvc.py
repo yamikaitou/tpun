e00c7ff7043e1250130fdfb36a68da292d4b02cd
Joseph <jnjbros2@gmail.com>
2022-08-18 11:43:34 -0700

    Reverted changes :/

--- pvc/pvc.py
+++ pvc/pvc.py
@@ -207,14 +207,13 @@ class pvc(commands.Cog):
                 try:
                     with open(str(vcOwnersPath), 'r') as vcOwners:
                         x = json.load(vcOwners)
-                        theVals: Generator = ([server, vcs, i, owner, vcId] for server, vcs in x.items() if server == str(ctx.guild.id) for i in vcs for owner, vcId in i.items())
-                        for valList in theVals:
-                            print(valList)
-                            print(valList[3])
-                            print(valList[4])
-                            if owner == str(valList[3]):
-                                await ctx.send("{0} You already have a vc created named {1}".format(ctx.author.name, str(self.bot.get_channel(valList[4]).name)))
-                                run = False
+                        for server, vcs in x.items():
+                            if server == str(ctx.guild.id):
+                                for i in vcs:
+                                    for owner, vcId in i.items():
+                                        if owner == str(owner):
+                                            await ctx.send("{0} You already have a vc created named {1}".format(ctx.author.name, str(self.bot.get_channel(vcId).name)))
+                                            run = False
                         if run:
                             channel = await ctx.guild.create_voice_channel(vcName, category=category)
                             await channel.set_permissions(ctx.author, view_channel=True, read_messages=True, send_messages=True, read_message_history=True, use_voice_activation=True, stream=True, speak=True, connect=True)
