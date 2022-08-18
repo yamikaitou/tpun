32979c3a8333c37f519e5fddc82ebb2d6950330b
Joseph <jnjbros2@gmail.com>
2022-08-18 10:36:00 -0700

    Forgot a semicolon

--- pvc/pvc.py
+++ pvc/pvc.py
@@ -208,7 +208,7 @@ class pvc(commands.Cog):
                         x = json.load(vcOwners)
                         theVals =([server, vcs, i, owner, vcId] for server, vcs in x.items() if server == str(ctx.guild.id) for i in vcs for owner, vcId in i.items())
                         for server, vcs, i, owner, vcId in theVals:
-                            if owner == str(owner)
+                            if owner == str(owner):
                                 await ctx.send("{0} You already have a vc created named {1}".format(ctx.author.name, str(self.bot.get_channel(theVals[vcId]).name)))
                                 run = False
                         if run:
