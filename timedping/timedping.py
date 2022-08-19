24c182559304a8a7bebca21f70ce229f1f23050d
Joseph <jnjbros2@gmail.com>
2022-08-19 11:35:35 -0700

    Fixed timed ping spamming console
    Every time someone sent the bot a message in dms it would spam AttributeError: 'NoneType' object has no attribute 'id'

--- timedping/timedping.py
+++ timedping/timedping.py
@@ -49,36 +49,37 @@ class timedping(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         global tempo
-        guild = message.guild.id
-        roles = {}
-        notInTempo = True
-        if "@" in message.content:
-            try:
-                with open(str(pingListPath), 'r') as pingList:
-                    x = json.load(pingList)
-                    for server, rolesList in x.items():
-                        if server == str(guild):
-                            for i in rolesList:
-                                roles = i
-            except ValueError:
-                print("pingList.json failed to read")
-        for role, cooldown in roles.items():
-            if bool(re.search(message.guild.get_role(int(role)).name, message.content, flags=re.I | re.X)) or bool(re.search(message.guild.get_role(int(role)).name, message.content, flags=re.I)):
-                for x, y in tempo.items():
-                    if x == str(role):
-                        notInTempo = False
-                        if y > time.time():
-                            await message.reply("There is a {0} hour cooldown in between uses. There is <t:{1}:R> remaining in the cooldown".format(str(cooldown), int(y)))
+        if message.guild is not None:
+            guild = message.guild.id
+            roles = {}
+            notInTempo = True
+            if "@" in message.content:
+                try:
+                    with open(str(pingListPath), 'r') as pingList:
+                        x = json.load(pingList)
+                        for server, rolesList in x.items():
+                            if server == str(guild):
+                                for i in rolesList:
+                                    roles = i
+                except ValueError:
+                    print("pingList.json failed to read")
+            for role, cooldown in roles.items():
+                if bool(re.search(message.guild.get_role(int(role)).name, message.content, flags=re.I | re.X)) or bool(re.search(message.guild.get_role(int(role)).name, message.content, flags=re.I)):
+                    for x, y in tempo.items():
+                        if x == str(role):
+                            notInTempo = False
+                            if y > time.time():
+                                await message.reply("There is a {0} hour cooldown in between uses. There is <t:{1}:R> remaining in the cooldown".format(str(cooldown), int(y)))
+                            else:
+                                await message.reply("<@&{0}>".format(role))
+                                newTempo = {str(role): int(time.time() + cooldown)}
+                                tempo.update(newTempo)
                         else:
-                            await message.reply("<@&{0}>".format(role))
-                            newTempo = {str(role): int(time.time() + cooldown)}
-                            tempo.update(newTempo)
-                    else:
-                        notInTempo = True
-                if notInTempo:
-                    await message.reply("<@&{0}>".format(int(role)))
-                    newTempo = {str(role): int(time.time() + cooldown)}
-                    tempo.update(newTempo)
+                            notInTempo = True
+                    if notInTempo:
+                        await message.reply("<@&{0}>".format(int(role)))
+                        newTempo = {str(role): int(time.time() + cooldown)}
+                        tempo.update(newTempo)
 
     @commands.group(name="tping", help="Base command for all timed ping commands")
     async def tping(self, ctx):
