62d58ee7a6d40f1a5e15cf455b427ae205f86312
Joseph <jnjbros2@gmail.com>
2022-08-20 15:28:35 -0700

    Fixed permissions

--- pingeveryone/pingeveryone.py
+++ pingeveryone/pingeveryone.py
@@ -18,7 +18,7 @@ class pingeveryone(commands.Cog):
             force_registration=True,
         )
 
-    @commands.admin()
+    @commands.admin_or_permissions(discord.Permissions.mention_everyone)
     @commands.command(name="pingeveryone", help="This command just ping everyone")
     async def pingeveryone(self, ctx: commands.Context,):
         allowed_mentions = discord.AllowedMentions(everyone = True)
