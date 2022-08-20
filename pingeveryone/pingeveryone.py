56027b33dc02f279f76d422d27b24a15a08f2252
Joseph <jnjbros2@gmail.com>
2022-08-20 15:20:39 -0700

    Update pingeveryone.py

--- pingeveryone/pingeveryone.py
+++ pingeveryone/pingeveryone.py
@@ -18,7 +18,7 @@ class pingeveryone(commands.Cog):
             force_registration=True,
         )
 
-    @commands.admin_or_permissions(discord.Permissions.mention_everyone)
+    @commands.admin_or_permissions()
     @commands.command(name="pingeveryone", help="This command just ping everyone")
     async def pingeveryone(self, ctx: commands.Context,):
         guild: discord.Guild = ctx.guild
