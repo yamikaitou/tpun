c5d985d6a1af5ea0781dd0452154a4d175521ad1
Joseph <jnjbros2@gmail.com>
2022-08-24 14:37:53 -0700

    Added true members

--- serverhud/serverhud.py
+++ serverhud/serverhud.py
@@ -25,15 +25,45 @@ class serverhud(commands.Cog):
         default_guild = {
             "channeltotmem": 0,
             "channelnewmem": 0,
+            "channeltruemem": 0,
             "channeltotbot": 0
         }
         self.config.register_guild(**default_guild)
 
     async def members(self, guild: discord.Guild):
-        channelId: int = await self.config.guild(guild).channeltotmem()
-        channel: discord.ChannelType = guild.get_channel(channelId)
-        await channel.edit(name='❎ MEMBERS: {} ❎'.format(guild.member_count))
+        true_member_count = len([m for m in ctx.guild.members if not m.bot])
+        totmem = guild.member_count
+        totmemId: int = await self.config.guild(guild).channeltotmem()
+        if totmemId != 0:
+            channel: discord.ChannelType = guild.get_channel(totmemId)
+            await channel.edit(name='❎ MEMBERS: {} ❎'.format(totmem))
+        
+        newmemId: int = await self.config.guild(guild).channelnewmem()
+        if newmemId != 0:
+            channel: discord.ChannelType = guild.get_channel(newmemId)
+            prefix: str = ""
+            Name: str = "New Members"
+            suffix: str = ""
+            newmembers: int = 0
+            await channel.edit(name='{0} {1} : {2} {3}'.format(prefix, Name, newmembers, suffix))
         await asyncio.sleep(300)
+
+        truememId: int = await self.config.guild(guild).channeltruemem()
+        if truememId != 0:
+            channel: discord.ChannelType = guild.get_channel(truememId)
+            prefix: str = ""
+            Name: str = "Users"
+            suffix: str = ""
+            await channel.edit(name='{0} {1} : {2} {3}'.format(prefix, Name, true_member_count, suffix))
+        
+        totbotId: int = await self.config.guild(guild).channeltotbot()
+        if totbotId != 0:
+            channel: discord.ChannelType = guild.get_channel(totbotId)
+            prefix: str = ""
+            Name: str = "Bots"
+            suffix: str = ""
+            bot_count: int = totmem - true_member_count
+            await channel.edit(name='{0} {1} : {2} {3}'.format(prefix, Name, bot_count, suffix))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -59,7 +89,7 @@ class serverhud(commands.Cog):
         For a list of channel types use [p]serverhud types
         """
         
-        types = ["newmem", "totmem", "totbot"]
+        types = ["newmem", "totmem", "totbot", "truemem"]
         for x in types:
             if x == type:
                 if x == "newmem":
@@ -71,6 +101,9 @@ class serverhud(commands.Cog):
                 elif x == "totbot":
                     await self.config.guild(ctx.guild).channeltotbot.set(channel)
                     await ctx.send("The total bot count channel has been set to <#{}>".format(channel))
+                elif x == "truemem":
+                    await self.config.guild(ctx.guild).channeltruemem.set(channel)
+                    await ctx.send("The True member count channel has been set to <#{}>".format(channel))
             else:
                 pass
         pass
@@ -82,7 +115,7 @@ class serverhud(commands.Cog):
 
         Use [p]serverhud setchannel <type> <channel mention>
         """
-        mess = "The avaible types of channels are: new members from today (newmem), total members (totmem), total bots (totbot)"
+        mess = "The avaible types of channels are: new members from today (newmem), total members (totmem), total bots (totbot), True Members (truemem)"
         await ctx.send(mess)
         pass
 
