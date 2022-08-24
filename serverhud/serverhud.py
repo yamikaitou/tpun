b7d2150ce7d3233f2b4992cf6f7c0298f496e5a2
Joseph <jnjbros2@gmail.com>
2022-08-24 16:35:50 -0700

    Update serverhud.py

--- serverhud/serverhud.py
+++ serverhud/serverhud.py
@@ -234,7 +234,7 @@ class serverhud(commands.Cog):
         """
         if event == "join" or event == "leave":
             await self.members(ctx.guild)
-            await ctx.send("The server channels being tested are: {0}".format(await self.config.guild(ctx.guild).channeltotmem()))
+            await ctx.send("The server channels being tested are: {0}".format(await self.config.guild(ctx.guild).totmem()["channel"]))
             await ctx.send("Test of the member join/leave event.")
         else:
             await ctx.send("That is not a valid event do [p]help serverhud test for a list of events")
