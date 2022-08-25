4b5b1b5268a85477a9516ba34f9a56fd9be91893
Joseph <jnjbros2@gmail.com>
2022-08-25 14:05:08 -0700

    Update rolebuy.py

--- rolebuy/rolebuy.py
+++ rolebuy/rolebuy.py
@@ -91,9 +91,11 @@ class rolebuy(commands.Cog):
         """
         roles = ""
         i = await self.config.guild(ctx.guild).buyableroles()
-        print(i)
         for role, cost in i.items():
             roles = roles + "<@&{0}> with cost of {1} currency \n".format(role, cost)
-        mess1 = await ctx.send(roles)
+        if roles != "":
+            mess1 = await ctx.send(roles)
+        else:
+            mess1 = await ctx.send("There are no buyable roles set up yet")
         await asyncio.sleep(120)
         await mess1.delete()
