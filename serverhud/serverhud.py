d8882e529fc26e2c98792a16d9b0f9815c004235
Joseph <jnjbros2@gmail.com>
2022-08-24 17:02:55 -0700

    Update serverhud.py

--- serverhud/serverhud.py
+++ serverhud/serverhud.py
@@ -58,6 +58,7 @@ class serverhud(commands.Cog):
         totmemId = totmemDict["channel"]
         if totmemId != 0:
             channel: discord.ChannelType = guild.get_channel(totmemId)
+            print(totmemDict["prefix"])
             await channel.edit(name='{0} {1}: {2} {3}'.format(totmemDict["prefix"], totmemDict["name"], totmem, totmemDict["suffix"]))
         
         newmemObj = await self.config.guild(guild).newmem()
