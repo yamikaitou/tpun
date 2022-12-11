74ae23ea25e2be5e4cd2607132303321ca4e384b
batman202012 <jnjbros2@gmail.com>
2022-12-11 12:51:31 -0700

    Squashed commit of the following:
    commit 1bf9d63bfe4bb0a50b4a1e1a25cb15b0f2e643b0
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:49:32 2022 -0700
    
        This method does not need to be awaited
    
    commit 63d4e41ac139dade319025474c9546e8b8a1bc1e
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:45:27 2022 -0700
    
        Made it so it does't reply to commands using chatbot
    
    commit 6b5b1260dbe0ce77fd32dce4f23411d84bb1bc78
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:41:38 2022 -0700
    
        Update chatGPT.py
    
    commit 3d4fec70e317dd61e3025940369e098f8c65b2db
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:37:54 2022 -0700
    
        Added check to make sure message wasn't sent from bot
    
        God I have never shutdown a bot so fast in my life :(
    
    commit 9078cf73be31ab4abaaf34008f2524033d917842
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:31:58 2022 -0700
    
        Update chatGPT.py
    
    commit b0d3eb83417e8511b5a99afe255615e28606a4c5
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:29:21 2022 -0700
    
        Ran into an error so added handling
    
    commit 8a87339971048448e5076cca5efb145bdb4f62d4
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:22:16 2022 -0700
    
        Threading is not needed here
    
    commit 83ba6cdc4d76f530f9d5be21c1ab434568a575c0
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:19:23 2022 -0700
    
        Update chatGPT.py
    
    commit d2ddcec2f27b51e8242c571aa86e46ce76770841
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:13:10 2022 -0700
    
        Added more logging info
    
    commit 7b8833975d758ae443cd6764ef93007eca96e09b
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:07:42 2022 -0700
    
        Ran into a type error so I added handling for it
    
    commit d3b26dabcccccf58a70f974020b7191449410f8a
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 12:03:50 2022 -0700
    
        Passed through model variable even though it wasn't being used
    
    commit a00f4fbffb38d66dbff3a38a6be9130272eb59fb
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 11:52:33 2022 -0700
    
        Lots of changes (see commit notes)
    
        *Added settings to change model
        *Added channel whitelist
        *Added settings to enable bot reply
    
    commit ea4940c46bb4aaf810d42f6c41aac13cf3fe011d
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:43:02 2022 -0700
    
        Just seeing if I can get it to run on this model
    
    commit a0737b1b3b543e1a006dafc030ae372772365a06
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:27:52 2022 -0700
    
        Apparently threading is already included in python
    
    commit 4e3790ae5745affb89394ef1df0ca6b59b1b64eb
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:25:42 2022 -0700
    
        Added threading into the chatGPT bot
    
    commit d5ccd9ea616847eff96c0f3606fbd2da9738cfde
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:23:39 2022 -0700
    
        Added threading module as a req
    
    commit 56244358c6d9a68febed198bea17384180e89eab
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:21:52 2022 -0700
    
        Fixed handling when response is empty
    
    commit 090fc832df5c41442d894e5309909181da528bcc
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:13:06 2022 -0700
    
        Oops look like I commited this as a stashed change somehow
    
    commit 56f9202eb60e71fdbc8320a3589b85077bee6adb
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:11:38 2022 -0700
    
        Added info level logging
    
        Info level logging was added to solve bugs easier, in the future all logging will be added to only show when the bot is run with -debug flag
    
    commit fda7635d7dff299bd246f8871b9bab2049bea9a3
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 10:10:35 2022 -0700
    
        Added numpy as a requirement
    
    commit 77b72d3d0d30b7077ca12f61815cc70be0d9cff8
    Author: batman202012 <jnjbros2@gmail.com>
    Date:   Sun Dec 11 02:49:35 2022 -0700
    
        Added link for api key on failed key get

--- chatGPT/chatGPT.py
+++ chatGPT/chatGPT.py
@@ -1,5 +1,6 @@
 from redbot.core import data_manager
 from redbot.core import commands
+from redbot.core import checks
 from redbot.core.bot import Red
 from redbot.core.config import Config
 import discord
@@ -13,19 +14,28 @@ class chatGPT(commands.Cog):
     self.prompt = ""
     self.response = ""
     self.bot = bot
-    self.log = logging.getLogger('red.tpun.occupations')
+    self.log = logging.getLogger('red.tpun.chatGPT')
     self.config = Config.get_conf(
         self,
         identifier=365398642334498816
     )
     self.user_threads = {}
-    
-  def send_message(self, user_id, message):
+    default_global = {
+        "model": "text-ada-001"
+    }
+    default_guild = {
+        "channels": [],
+        "replyRespond": True
+    }
+    self.config.register_global(**default_global)
+    self.config.register_guild(**default_guild)
+
+  def send_message(self, user_id, message, model):
     if user_id not in self.user_threads:
       self.user_threads[user_id] = ""
     self.prompt = self.user_threads[user_id]
     response = openai.Completion.create(
-      engine="text-davinci-002",
+      engine=model,
       prompt=self.prompt + message,
       max_tokens=1024,
       n=1,
@@ -35,22 +45,147 @@ class chatGPT(commands.Cog):
     self.user_threads[user_id] = response["choices"][0]["text"]
     return self.user_threads[user_id]
 
-  @commands.command(name="chatgpt")
-  async def chatgpt(self, ctx: commands.Context, *, query: str):
+  async def send_chat(self, ctx: commands.Context, query: str):
+    async with ctx.typing():
+        model = await self.config.model()
+        self.log.info("Sending query: `" + query + "` to chatGPT. With model: " + model)
+        chatGPTKey = await self.bot.get_shared_api_tokens("openai")
+        if chatGPTKey.get("api_key") is None:
+            self.log.error("No api key set.")
+            return await ctx.send("The bot owner still needs to set the openai api key using `[p]set api openai  api_key,<api key>. It can be created at: https://beta.openai.com/account/api-keys`")
+        openai.api_key = chatGPTKey.get("api_key")
+        response: str = self.send_message(ctx.author.id, query, model)
+        if len(response) > 0 and len(response) < 2000:
+            self.log.info("Response is under 2000 characters and is: `" + response + "`.")
+            await ctx.reply(response)
+        elif len(response) > 2000:
+            self.log.info("Response is over 2000 characters sending as file attachment. Response is: `" + response + "`.")
+            with open(str(ctx.author.id) + '.txt', 'w') as f:
+                f.write(response)
+            with open(str(ctx.author.id) + '.txt', 'r') as f:
+                await ctx.send(file=discord.File(f))
+                os.remove(f)
+        else:
+            await ctx.reply("I'm sorry, for some reason chatGPT's response contained nothing, please try sending your query again.")
+
+  @commands.Cog.listener()
+  async def on_message_without_command(self, message: discord.Message):
+    whitelistedChannels: list = await self.config.guild(message.guild).channels()
+    replyRespond: bool = await self.config.guild(message.guild).replyRespond()
+    query = message.content
+    ctx = await self.bot.get_context(message)
+    if whitelistedChannels is not None:
+        if message.channel.id in whitelistedChannels and message.author.id != self.bot.user.id:
+            await self.send_chat(ctx, query)
+    if replyRespond and message.reference is not None and message.author.id != self.bot.user.id:
+        if message.reference.cached_message is None:
+            # Fetching the message
+            channel = self.bot.get_channel(message.reference.channel_id)
+            msg = await channel.fetch_message(message.reference.message_id)
+            context: commands.Context = await self.bot.get_context(msg)
+
+        else:
+            msg = message.reference.cached_message
+            context: commands.Context = await self.bot.get_context(msg)
+        if context.author.id == self.bot.user.id:
+            await self.send_chat(ctx, query)
+
+  @commands.group(name="chatgpt")
+  async def chatgpt(self, ctx: commands.Context):
+        """
+        Base command for chatgpt related commands
+        """
+        pass
+
+  @chatgpt.command(name="chat")
+  async def chat(self, ctx: commands.Context, *, query: str):
     """
     Asks chatgpt a query
     """
-    async with ctx.typing():
-      chatGPTKey = await self.bot.get_shared_api_tokens("openai")
-      if chatGPTKey.get("api_key") is None:
-        return await ctx.send("The bot owner still needs to set the openai api key using `[p]set api openai  api_key,<api key>`")
-      openai.api_key = chatGPTKey.get("api_key")
-      response : str = self.send_message(ctx.author.id, query)
-      if len(response) < 2000:
-        await ctx.reply(response)
-      else:
-        with open(str(ctx.author.id) + '.txt', 'w') as f:
-            f.write(response)
-        with open(str(ctx.author.id) + '.txt', 'r') as f:
-            await ctx.send(file=discord.File(f))
-            os.remove(f)
+    await self.send_chat(ctx, query)
+
+  @checks.guildowner()
+  @chatgpt.command(name="set")
+  async def set(self, ctx: commands.Context, setting: str, value: str):
+    """
+    Changes settings for bot to use
+
+    Use [p]chatgpt set channeladd <channel_id> or [p]chatgpt set channelremove <channel_id> to set up channel whitelist where the bot will respond.
+    Use [p]chatgpt set replyRespond <True or False> to enable or disable the bot responding to replies regardless of channel
+    """
+    if setting == "channeladd":
+        channelId = int(value)
+        channel = self.bot.get_channel(channelId)
+        if channel == None:
+            ctx.reply("That channel does not exist or the bot can not see it.")
+        elif channel.guild != ctx.guild:
+            ctx.reply("That channel isn't in this server...")
+        else:
+            currentChannels: list = await self.config.guild(ctx.guild).channels()
+            if currentChannels is None:
+                newChannels: list = [channelId]
+                await ctx.reply("<#" + str(channelId) + "> is now whitelisted.")
+                await self.config.guild(ctx.guild).channels.set(newChannels)
+            else:
+                if channelId not in currentChannels:
+                    newChannels: list = currentChannels.append(channelId)
+                    await ctx.reply("<#" + str(channelId) + "> is now whitelisted.")
+                    await self.config.guild(ctx.guild).channels.set(newChannels)
+                else:
+                    await ctx.reply("<#" + str(channelId) + "> was already whitelisted.")
+            
+            
+
+    elif setting == "channelremove":
+        currentChannels: list = await self.config.guild(ctx.guild).channels()
+        try:
+            newChannels = currentChannels.remove(value)
+            await self.config.guild(ctx.guild).channels.set(newChannels)
+            await ctx.reply("<#" + str(channelId) + "> is no longer whitelisted.")
+        except ValueError:
+            newChannels = currentChannels
+            ctx.reply("That channel was already not in channel list.")
+
+    elif setting == "replyRespond":
+        if value == "true" or value == "True" or value == "1":
+            await self.config.guild(ctx.guild).replyRespond.set(True)
+            await ctx.reply("replyRespond is now set to True")
+        elif value == "false" or value == "False" or value == "0":
+            await self.config.guild(ctx.guild).replyRespond.set(False)
+            await ctx.reply("replyRespond is now set to False")
+
+  @checks.is_owner()
+  @chatgpt.command(name="model")
+  async def model(self, ctx: commands.Context, model: str):
+    """
+    Allows the changing of model chatbot is running options are: 0-`text-ada-001` 1-`text-babbage-001` 2-`text-curie-001` 3-`text-davinci-002` 4-`text-davinci-002-render` 5-`text-davinci-003` current-`shows current model`
+
+    For more information on what this means please check out: https://beta.openai.com/docs/models/gpt-3
+    """
+    if model == "0" or model == "text-ada-001":
+        await self.config.model.set("text-ada-001")
+        await ctx.reply("The chatbot model is now set to: `text-ada-001`")
+
+    elif model == "1" or model == "text-babbage-001":
+        await self.config.model.set("text-babbage-001")
+        await ctx.reply("The chatbot model is now set to: `text-babbage-001`")
+
+    elif model == "2" or model == "text-curie-001":
+        await self.config.model.set("text-curie-001")
+        await ctx.reply("The chatbot model is now set to: `text-curie-001`")
+
+    elif model == "3" or model == "text-davinci-002":
+        await self.config.model.set("text-davinci-002")
+        await ctx.reply("The chatbot model is now set to: `text-davinci-002`")
+
+    elif model == "4" or model == "text-davinci-002-render":
+        await self.config.model.set("text-davinci-002-render")
+        await ctx.reply("The chatbot model is now set to: `text-davinci-002-render`")
+
+    elif model == "5" or model == "text-davinci-003":
+        await self.config.model.set("text-davinci-003")
+        await ctx.reply("The chatbot model is now set to: `text-davinci-003`")
+
+    elif model == "current":
+        currentModel = await self.config.model()
+        await ctx.reply("The chatbot model is currently set to: " + currentModel)
